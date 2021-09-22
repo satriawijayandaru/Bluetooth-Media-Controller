96a65818c46bdecd9fc3dad5e6c95c04c2853918
vrey11 <yanda1998@gmail.com>
2021-09-23 03:37:51 +0700

    first commit

---
+++ bluetooth_media_control_with_rotary/bluetooth_media_control_with_rotary.ino
@@ -0,0 +1,15 @@
+#include <BleKeyboard.h>
+
+BleKeyboard bleKeyboard;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+  bleKeyboard.begin();
+}
+
+void loop() {
+  if (bleKeyboard.isConnected()) {
+
+  }
+}
